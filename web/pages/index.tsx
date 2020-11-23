ed8668219b73f51be6cf0e14a6bbe077a2df4089
Jason Chen <jasonc1@andrew.cmu.edu>
2020-11-22 23:54:12 -0800

    first pass

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,11 +1,12 @@
-import SignUp from './SignUp/SignUpPage';
-import Link from 'next/link'
+import SignUp from "./SignUp/SignUpPage";
+import Link from "next/link";
+import { SignUpButton } from "../components/sign-up-button/sign-up-button.component";
 
 export const Index = () => {
   return (
     <p>
       <Link href="./SignUp/SignUpPage">
-        <a> SignUp </a>
+        <SignUpButton buttonText="sign up" />
       </Link>
     </p>
   );
