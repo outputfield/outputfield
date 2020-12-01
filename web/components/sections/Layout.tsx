268d7ef17b6d132a8b4a66783e447899d1e60bb3
Max Bendick <maxbendick@gmail.com>
2020-12-01 10:57:34 -0800

    Fix build

--- web/components/sections/Layout.tsx
+++ web/components/sections/Layout.tsx
@@ -1,1 +1,7 @@
 //TODO create a overall Layout file for global layout.
+export const zzzASDFKSJDF = `
+adding this export to fix this build error:
+
+./components/sections/Layout.tsx:1:1
+Type error: All files must be modules when the '--isolatedModules' flag is provided.
+`;
