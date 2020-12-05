5d9b0272ea53a2c9279271f1158bf0111ea356e8
Jason Chen <jasonc1@andrew.cmu.edu>
2020-12-05 12:07:19 -0800

    design tokens added - need to verify

--- web/colors.js
+++ web/colors.js
@@ -1,7 +1,11 @@
 // This file is so that we can pass hexcodes in Javascript/Typscript. It requires alot of work to be able to export from SCSS to Typescript
 export const theme = {
-  white: '#fff',
-  black: '#000000',
+  white: "#fff",
+  black: "#000000",
+  primary: "#0000FF",
+  error: "#FF0000",
+  magnetic: "#E0FF00",
+  disabled: "#7B61FF",
 };
 
 export default theme;
