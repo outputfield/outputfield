4809954aae135fa28485035bd4fb2a357d80b9ad
Max Bendick <maxbendick@gmail.com>
2020-12-27 10:45:50 -0800

    Use TextInput in landing page

--- web/pages/landing.tsx
+++ web/pages/landing.tsx
@@ -1,25 +1,18 @@
-import React, { useState } from "react";
 import axios from "axios";
+import React, { useState } from "react";
 import { getErrorMessage } from "../api-client/errors";
-import { sendSignup, SignupRequest } from "../api-client/signup";
-import {
-  Input,
-  Label,
-  SignUpForm,
-  Intro
-} from "../components/landingPage";
-import { Text } from "../components/text/text.component";
+import colors from "../colors";
 import { Cursor } from "../components/cursor/cursor.component";
 import { Donut } from "../components/donut/donut.component";
+import { Intro, SignUpForm } from "../components/landingPage";
 import { SignUpButton } from "../components/sign-up-button/sign-up-button.component";
-import colors from "../colors";
+import { TextInput } from "../components/text-input/text-input.component";
+import { Text } from "../components/text/text.component";
 
 const page = "Frontpage";
 
 const Landing = (props) => {
-  const {
-    pageData
-  } = props
+  const { pageData } = props;
   /**
   Resembles the landing page.
     */
@@ -41,38 +34,50 @@ const Landing = (props) => {
     }
   };
 
+  const isError = state === "error";
+
   return (
     <Cursor color="#FF0000" strokeLength={20}>
-      <Intro>{pageData.introduction.map((t)=>{return <Text text={t} size={"H1"}/>})}</Intro>
-      <Donut text="OUTPUT FIELD"/>
+      <Intro>
+        {pageData.introduction.map((t) => {
+          return <Text text={t} size={"H1"} />;
+        })}
+      </Intro>
+      <Donut text="OUTPUT FIELD" />
       <SignUpForm onSubmit={subscribe}>
-        <Input
-          value={email}
-          type="text"
-          onChange={(event) =>
-            setRegisterData(event.target.value)
-          }
-        ></Input>
+        <TextInput
+          label={"Sign up for launch updates"}
+          onChange={(event) => setRegisterData(event.target.value)}
+          invalid={isError}
+          errorMessage={isError && error ? error : undefined}
+        />
+        {state === "success" && (
+          <Text
+            text="Subscribing successful. Stay tuned."
+            color={colors.success}
+            size="H2"
+            marginTop={10}
+          />
+        )}
 
-        {state === "" && (<Text text="Sign up for launch updates" size="H2" marginTop={47} />)}
-        {state === "error" && (<Text text={error} color={colors.error} size="H2" marginTop={47} />)}
-        {state === "success" && (<Text text="Subscribing successful. Stay tuned." color={colors.success} size="H2" marginTop={47} />)}
-
-        <SignUpButton buttonText="sign up" marginLeft={450} marginTop={-100}/>
-
+        <SignUpButton buttonText="sign up" marginLeft={450} marginTop={-100} />
       </SignUpForm>
       <script> </script> {/*chrome form transition bug fix*/}
     </Cursor>
-
-  )
+  );
 };
 
 Landing.getInitialProps = async function (context) {
-  try{
-    const response = await axios.get(`${context.req !== undefined?process.env.SERVER_URI:""}/api/page-content`,{params:{page:page}});
-    return {pageData: response.data};
+  try {
+    const response = await axios.get(
+      `${
+        context.req !== undefined ? process.env.SERVER_URI : ""
+      }/api/page-content`,
+      { params: { page: page } }
+    );
+    return { pageData: response.data };
   } catch (event) {
     throw getErrorMessage(event);
   }
-}
+};
 export default Landing;
