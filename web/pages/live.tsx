6a845b3d38bcdfac3f3ca20e99434533412ae88a
Evan Burgess <evan.a.burgess@gmail.com>
2021-03-28 11:19:09 -0400

    updated live page content

--- web/pages/live.tsx
+++ web/pages/live.tsx
@@ -50,7 +50,7 @@ const Live = (props) => {
     }
     setMessage(m);
     messageRef.current = m;
-    if(!liveRef.current){
+    if(r.live != liveRef.current){
       setLive(r.live);
       liveRef.current = r.live;
     }
@@ -111,14 +111,17 @@ const Live = (props) => {
         <div className={styles.detailgrid}>
           <div className={styles.detailtitle}>Critical Broadcast: Stop AAPI Hate Fundraiser</div>
           <div className={styles.detaildescription}>
-            Insights from writers and activists that explore the challenges and rewards of naming political solidarity, and bridging theories of race to everyday life and activism. If you hear or see something that resonates, please consider donating. Your contibution will be doubled.<br/>
-            <br/>
-            From Anne Anlin Cheng’s recent call for a theory of the “yellow woman” to Grace Lee Boggs’ echoing insistence that we can only change society if we take responsibility for that change, we’ll be sharing voices that express the diversity of what it looks like to be political. Excavate the covert, subvert the inert. Jex Wang (Easter Margins) will conclude with an hour of ambient sound. Tracklist can be found <a href={pageData.tracklist}>here</a>.
+          Insights from writers and activists that explore the challenges and rewards of naming political solidarity, and bridging theories of race to everyday life and activism. If you hear or see something that resonates, please consider donating. We will be matching donations until April 4th. Your contribution will be doubled and sent to <a href="https://stopaapihate.org/">Stop AAPI Hate</a>, a coalition addressing anti-Asian hate amid the pandemic.<br/>
+          <br/>
+          From Anne Anlin Cheng’s recent call for a theory of the “yellow woman” to Grace Lee Boggs’ echoing insistence that we can only change society if we take responsibility for that change, we’ll be sharing voices that express the diversity of what it looks like to be political. Excavate the covert, subvert the inert. Jex Wang (Easter Margins) will conclude with an hour of ambient sound.<br/>
+          <br/>
+          Full transcript with linked videos can be found <a href="https://www.notion.so/Critical-Broadcast-Info-7aa6cc9324054a7085369ea612714cc7">here</a>, along with the Jex's tracklist. Sign up for updates on other generative, critical, speculative events at <a href="http://outputfield.com/">outputfield.com</a>.
           </div>
           <div className={styles.detailcredit}>
-          Visuals by Alejandro Zhang<br/>
-          Music by Jex Wang<br/>
-          Curatied by Clelia Knox/Vivian Qiu
+          Organized by: <a href="https://www.vivianqiu.com/">Vivian Qiu</a><br/>
+          Curated by: <a href="https://vimeo.com/user50752539">Clelia Knox</a><br/>
+          Visuals by: <a href="https://www.instagram.com/alejandro_zhang/">Alejandro Jūnyáo Zhāng</a><br/>
+          Sound by: <a href="https://www.instagram.com/asiangirlfriend/">Jex Wang</a>
           </div>
           <div className={styles.detailbutton}>
             <a href="https://www.paypal.com/pools/c/8xZf4atFjK" target="_blank"><SignUpButton className={"livepage"} buttonText="DONATE"/></a>
