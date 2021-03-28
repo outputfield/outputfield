2236dacd88178d9094c43e0d8439d4d9a7c580e3
Evan Burgess <evan.a.burgess@gmail.com>
2021-03-28 01:34:31 -0400

    update image + meta tags

--- web/pages/live.tsx
+++ web/pages/live.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
+import Head from 'next/head';
 import { getErrorMessage } from "../api-client/errors";
 import { getPageContent } from "./api/page-content";
 import styles from "../components/live/live.module.scss";
@@ -63,8 +64,38 @@ const Live = (props) => {
     }
   });
 
+
+
+  const meta_title = "Output Field";
+  const meta_description = "Critical Broadcast: Stop AAPI Hate Fundraiser";
+  const meta_canonical = "https://outputfield.com/live";
+  const meta_image = "https://outputfield.com/live/Screen_Donate.png";
+
   return (
     <div className={styles.root}>
+      <Head>
+        <style>{'html,body,#__next { background-color: #e5e5e5; }'}</style>
+      	<meta property="og:title" content={meta_title}/>
+      	<meta name="twitter:title" content={meta_title}/>
+      	<meta itemprop="name" content={meta_title}/>
+        <link rel="canonical" href={meta_canonical}/>
+        <meta property="og:url" content={meta_canonical}/>
+        <meta name="description" content={meta_description}/>
+        <meta itemprop="description" content={meta_description}/>
+        <meta property="og:description" content={meta_description}/>
+        <meta name="twitter:description" content={meta_description}/>
+        <meta property="og:type" content="website"/>
+        <meta name="twitter:card" content="summary_large_image"/>
+        <meta name="twitter:site" content=""/>
+        <meta name="twitter:image" content={meta_image}/>
+        <meta property="og:image" content={meta_image}/>
+        <meta itemprop="image" content={meta_image}/>
+        <meta property="og:image:alt" content={meta_title}/>
+        <link rel="shortcut icon" href="https://outputfield.com/meta/favicon.ico"/>
+        <link rel="apple-touch-icon" sizes="180x180" href="https://outputfield.com/meta/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="https://outputfield.com/meta/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="https://outputfield.com/meta/meta/favicon-16x16.png"/>
+      </Head>
       <div className={styles.grid}>
         {message != ""?
           <div className={styles.donation}>{message}</div>
@@ -75,14 +106,14 @@ const Live = (props) => {
           {live==true?
             <iframe ref={video} width="100%" height="100%" src={pageData.youtube} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             :
-            <div className={styles.image}><img className={styles.illustration} src="live/Screen Shot 2021-03-23 at 7.38 1.png"/><img className={styles.inset} src="live/Screen Shot 2021-03-23 at 7.40 1.png"/></div>}
+            <div className={styles.image}><img className={styles.illustration} src="live/Screen_Donate.png"/></div>}
         </div>
         <div className={styles.detailgrid}>
           <div className={styles.detailtitle}>Critical Broadcast: Stop AAPI Hate Fundraiser</div>
           <div className={styles.detaildescription}>
             Insights from writers and activists that explore the challenges and rewards of naming political solidarity, and bridging theories of race to everyday life and activism. If you hear or see something that resonates, please consider donating. Your contibution will be doubled.<br/>
             <br/>
-            From Anne Anlin Cheng’s recent call for a theory of the “yellow woman” to Grace Lee Boggs’ echoing insistence that we can only change society if we take responsibility for that change, we’ll be sharing voices that express the diversity of what it looks like to be political. Excavate the covert, subvert the inert. Jex Wang (Easter Margins) will conclude with an hour of ambient sound. Tracklist can be found <a href="https://soundcloud.com">here</a>.
+            From Anne Anlin Cheng’s recent call for a theory of the “yellow woman” to Grace Lee Boggs’ echoing insistence that we can only change society if we take responsibility for that change, we’ll be sharing voices that express the diversity of what it looks like to be political. Excavate the covert, subvert the inert. Jex Wang (Easter Margins) will conclude with an hour of ambient sound. Tracklist can be found <a href={pageData.tracklist}>here</a>.
           </div>
           <div className={styles.detailcredit}>
           Visuals by Alejandro Zhang<br/>
