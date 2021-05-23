eca998ebf6e297c5234b9aa0631b6271569d8a46
Max Bendick <maxbendick@gmail.com>
2021-05-22 19:26:02 -0700

    Desktop looks nice

--- web/pages/rsvp.tsx
+++ web/pages/rsvp.tsx
@@ -4,8 +4,11 @@ import React from "react";
 import "tailwindcss/tailwind.css";
 import { SignUpButton } from "../components/sign-up-button/sign-up-button.component";
 
-const Link: React.FC<{ className?: string }> = ({ className, children }) => {
-  const color = "#0404FF";
+const Link: React.FC<{
+  className?: string;
+  href?: string;
+  target?: string;
+}> = ({ className, children, href, target }) => {
   return (
     <a
       className={classnames(
@@ -16,6 +19,8 @@ const Link: React.FC<{ className?: string }> = ({ className, children }) => {
         color: "rgb(4, 4, 255)",
         textShadow: `0 0 2px rgba(4, 4, 255, 0.4)`,
       }}
+      href={href}
+      target={target}
     >
       {children}
     </a>
@@ -24,29 +29,20 @@ const Link: React.FC<{ className?: string }> = ({ className, children }) => {
 
 const Logo: React.FC<{ className?: string }> = ({ className }) => {
   return (
-    <div className={classnames("h-40", className)}>
-      <div className="relative top-10  flex justify-center">
-        <div className="font-serif italic text-xl w-min">Skin Garden</div>
-      </div>
-
-      <div className="relative -top-16">
-        <Image
-          src="/rsvp-deco.png"
-          alt="decorative arc"
-          height="200"
-          width="222"
-        />
-      </div>
-    </div>
+    <Image src="/SGLogo.png" alt="decorative arc" height="200" width="222" />
   );
 };
 
 const Header: React.FC<{ className?: string }> = ({ className }) => {
   return (
     <div className={classnames("p-5 text-center", className)}>
-      <Logo className="mb-6" />
-      <Link className="uppercase">Output Field</Link>
-      <div className="uppercase text-lg">Debut showcase</div>
+      <div className="text-center flex flex-col">
+        <div>
+          <Logo className="mb-6" />
+        </div>
+        <Link className="uppercase" href="/" target="_blank">Output Field</Link>
+        <div className="uppercase text-lg">Debut showcase</div>
+      </div>
     </div>
   );
 };
@@ -59,18 +55,36 @@ const Day: React.FC<{
   className?: string;
   day: string;
   date: string;
+  time: string;
+  rsvpLink: string;
   room: ReturnType<React.FC>;
   happening: ReturnType<React.FC>;
-  when: ReturnType<React.FC>;
   who: ReturnType<React.FC>;
   description: ReturnType<React.FC>;
-}> = ({ className, day, date, room, happening, when, who, description }) => {
+}> = ({
+  className,
+  day,
+  date,
+  time,
+  room,
+  happening,
+  rsvpLink,
+  who,
+  description,
+}) => {
   return (
     <div className={classnames("", className)}>
       <div className="pb-6">
-        <div className="font-serif italic text-gray-700 pb-1">{day}</div>
-        <div className="font-serif italic text-4xl pb-2">{date}</div>
-        <Link className="mb-6">RSVP</Link>
+        <div className="font-serif italic text-gray-700 pb-2 text-base">
+          {day}
+        </div>
+        <div className="font-serif italic text-4xl pb-2">
+          {date}
+          <span className="text-base pl-4">{time}</span>
+        </div>
+        <Link className="mb-6" href={rsvpLink} target="_blank">
+          RSVP
+        </Link>
       </div>
       <div className="h-24 text-lg leading-snug">
         <DaySubheader>What Room is Opening?</DaySubheader>
@@ -80,15 +94,11 @@ const Day: React.FC<{
         <DaySubheader>What else is Happening?</DaySubheader>
         {happening}
       </div>
-      <div className="h-20 text-lg leading-snug">
-        <DaySubheader>When?</DaySubheader>
-        {when}
-      </div>
       <div className="h-28 text-lg leading-snug">
         <DaySubheader>Who?</DaySubheader>
         {who}
       </div>
-      <div className="text-lg leading-snug">{description}</div>
+      <div className="text-md leading-snug">{description}</div>
     </div>
   );
 };
@@ -98,10 +108,11 @@ const Wednesday: React.FC<{ className?: string }> = ({ className }) => {
     <Day
       className={classnames("", className)}
       day="Wednesday"
+      time="3PM PST"
       date="6/16"
+      rsvpLink="https://skingarden-day1.splashthat.com/"
       room={<div>Skin Garden Lobby & Walk-ins Welcome</div>}
       happening={<div>DJ sets hosted by Bien Agiter</div>}
-      when={<div>3PM PST on Wednesday</div>}
       who={<div>Soft Matter, Online Threat, +</div>}
       description={
         <div>
@@ -121,9 +132,10 @@ const Thursday: React.FC<{ className?: string }> = ({ className }) => {
       className={classnames("", className)}
       day="Thursday"
       date="6/17"
+      time="9AM PST"
+      rsvpLink="https://skingarden-day2.splashthat.com/"
       room={<div>Bodies Unhinge</div>}
       happening={<div>Panel Discussion, Q&A</div>}
-      when={<div>9AM PST on Thursday</div>}
       who={<div>Betty Apple, IOR50, Venus in Foil</div>}
       description={
         <div>
@@ -143,9 +155,10 @@ const Friday: React.FC<{ className?: string }> = ({ className }) => {
       className={classnames("", className)}
       day="Friday"
       date="6/18"
+      time="11AM PST"
+      rsvpLink="https://skingarden-day3.splashthat.com/"
       room={<div>Reconsider Flesh</div>}
       happening={<div>Listening Session (Theory for context)</div>}
-      when={<div>11AM on Wednesday</div>}
       who={<div>TBA+</div>}
       description={
         <div>
@@ -164,9 +177,9 @@ const Rsvp = () => {
     <div className="bg-gray-300 min-h-screen flex justify-center pt-20">
       <div className="container">
         <div className="grid grid-rows-1 grid-auto-cols gap-10">
-          <Header className="row-start-1 col-start-1" />
+          <Header className="row-start-1 col-start-1 w-64" />
           <div className="row-start-1 col-start-2 ">
-            <div className="grid grid-auto-rows grid-cols-rsvp gap-20">
+            <div className="grid grid-auto-rows grid-cols-rsvp gap-x-16 gap-y-10">
               <Wednesday className="col-start-1 row-start-1" />
               <Thursday className="col-start-2 row-start-1" />
               <Friday className="col-start-3 row-start-1" />
@@ -177,7 +190,7 @@ const Rsvp = () => {
                 </div>
               </div>
               <div className="col-start-3 row-start-2">
-                <div className="text-center">
+                <div className="text-center mt-3">
                   <SignUpButton buttonText="Donate"></SignUpButton>
                 </div>
               </div>
