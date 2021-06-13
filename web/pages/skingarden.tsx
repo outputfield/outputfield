f43c12fa4374413a962e7e88fb55567610927844
Max Bendick <maxbendick@gmail.com>
2021-06-13 14:11:33 -0700

    Website improvements

--- web/pages/skingarden.tsx
+++ web/pages/skingarden.tsx
@@ -1,6 +1,6 @@
 import classnames from "classnames";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import "tailwindcss/tailwind.css";
 import { SignUpButton } from "../components/sign-up-button/sign-up-button.component";
 import { Artist } from "../components/skingarden/artist";
@@ -29,7 +29,7 @@ const Content: React.FC<{ className?: string; innerClassName?: string }> = ({
   children,
 }) => {
   return (
-    <div className={classnames(className, "flex justify-center px-6")}>
+    <div className={classnames(className, "flex justify-center px-6 sm:px-10")}>
       <div className={classnames(innerClassName, "w-full max-w-4xl")}>
         {children}
       </div>
@@ -53,10 +53,12 @@ const ArtistGroup: React.FC<{ title?: string; className?: string }> = ({
   );
 };
 
-const Section: React.FC<{ title: string }> = ({ title, children }) => {
+const Section: React.FC<{ title?: string }> = ({ title, children }) => {
   return (
-    <div className="pb-40">
-      <h2 className="text-3xl sm:text-4xl pb-6 font-serif">{title}</h2>
+    <div className="pb-48 last:pb-24">
+      {title ? (
+        <h2 className="text-3xl sm:text-4xl pb-6 font-serif">{title}</h2>
+      ) : null}
       <div className="space-y-24">{children}</div>
       <div className="text-center pt-20">
         <DonateButton />
@@ -73,21 +75,127 @@ const HeaderLink: React.FC<{ className?: string }> = ({
     <a
       className={classnames(
         className,
-        "text-4xl sm:text-6xl font-bold text-black block py-2 border-b border-dashed"
+        "text-4xl sm:text-6xl font-bold text-black block py-1 sm:py-2 border-b border-dashed first:border-t"
       )}
     >
       <Content>{children}</Content>
     </a>
   );
 };
+const HeaderDate: React.FC<{ className?: string }> = ({
+  children,
+  className,
+}) => {
+  return (
+    <div className={classnames("uppercase font-bold text-lg", className)}>
+      {children}
+    </div>
+  );
+};
 const Header: React.FC = () => {
   return (
     <div className="">
-      <HeaderLink>RSVP</HeaderLink>
-      <HeaderLink>Show Info</HeaderLink>
-      <HeaderLink>Transcripts</HeaderLink>
-      <HeaderLink>Donate</HeaderLink>
-      <HeaderLink className="bg-yellow-300">Enter the garden</HeaderLink>
+      <div
+        className="
+        w-full flex justify-center
+        sm:absolute"
+      >
+        <div className="sm:w-full sm:max-w-xl sm:text-right sm:pt-20">
+          <Image
+            src="/SGLogo.png"
+            alt="Sking garden logo"
+            height="200"
+            width="222"
+            className="z-10"
+          />
+        </div>
+      </div>
+
+      <Content>
+        <div
+          className="
+          flex space-x-8 justify-center pb-8 sm:pb-0
+          sm:justify-start sm:absolute sm:pl-52 sm:pt-6"
+        >
+          <HeaderDate className="text-red-500 transform -rotate-3">
+            Wed 6/16
+          </HeaderDate>
+          <HeaderDate className="transform rotate-12">Thur 6/17</HeaderDate>
+          <HeaderDate className="transform -rotate-12">Fri 6/18</HeaderDate>
+        </div>
+      </Content>
+      <div>
+        <HeaderLink>RSVP</HeaderLink>
+        <HeaderLink>Show Info</HeaderLink>
+        <HeaderLink>Transcripts</HeaderLink>
+        <HeaderLink>Donate</HeaderLink>
+        <HeaderLink className="bg-yellow-500">Enter the garden</HeaderLink>
+      </div>
+    </div>
+  );
+};
+
+const PreviewSelector: React.FC<{
+  title: string;
+  active?: boolean;
+  onClick?: () => void;
+  className?: string;
+}> = ({ title, active, onClick, className }) => {
+  return (
+    <div className={classnames("", className)}>
+      <div
+        className={classnames(
+          active && "",
+          "bg-green-300 h-52 w-40 cursor-pointer transition duration-200"
+        )}
+        style={
+          active
+            ? { boxShadow: "0px 0px 10px 6px rgba(0,0,255,0.8)" }
+            : undefined
+        }
+        onClick={() => onClick?.()}
+      ></div>
+      <div className="uppercase text-sm font-bold pt-4">{title}</div>
+    </div>
+  );
+};
+
+const previewContent = [
+  `Output Field presents its debut exhibition: Skin Garden, a collection of artworks discussing bodies: through movement, space, sound, self, and fellow bodies. Skin Garden is body language. The three rooms inside this lobby present audiovisual reflections on our physical vessels, as they relate to trauma, language, queerness, tradition, and multiplicity.`,
+  `Walk-ins Welcome is a digital tattoo showroom. Advances in image-rendering have become interwoven into the process of tattooing, and archived onto skin. More and more, this analog art form has seen a shift towards the digital and the experimental. Walk-ins Welcome is a showcase of artists who have embraced this shift in their practice.`,
+  `Bodies Unhinge is an articulation of queer affect, through breath, movement, and voice. IOR50 and Betty Apple explore the ways unruly aesthetic experiences disrupt the borders of flesh and shake loose the psychic debris left by trauma. Here, the queer dynamics of raving offer the club as a site to sound out the affective afterlife of psychic injury.`,
+  `Reconsider Flesh is a breathing sculpture garden nestled inside walls sculpted by Salomé Chatriot. Sourcing the multiplicity of flesh, this body of work carves out a space for just some of the bodies excluded when multitudes are reduced to a monolithic standard. These 3-dimensional musings are presented with Bella Baguena's distorted readings.`,
+];
+
+const Previews: React.FC = () => {
+  const [activePreview, setActive] = useState(0);
+  const content = previewContent[activePreview];
+
+  return (
+    <div className="pb-28">
+      <div className="flex justify-between">
+        <PreviewSelector
+          title="Bodies Unhinge"
+          active={activePreview === 0}
+          onClick={() => setActive(0)}
+        />
+        <PreviewSelector
+          title="The Lobby"
+          active={activePreview === 1}
+          onClick={() => setActive(1)}
+        />
+        <PreviewSelector
+          title="Reconsider Flesh"
+          active={activePreview === 2}
+          onClick={() => setActive(2)}
+        />
+        <PreviewSelector
+          title="Walk-ins Welcome"
+          active={activePreview === 3}
+          onClick={() => setActive(3)}
+        />
+      </div>
+      <div className="pt-8 text-lg">{content}</div>
     </div>
   );
 };
@@ -96,7 +204,10 @@ const SkinGarden = () => {
   return (
     <div className="text-black bg-gray-100">
       <Header />
+
       <Content className="pt-20">
+        <Previews />
+
         <Section title="Reconsider Flesh">
           <SpacedParagraphs text={skingardenText.reconsiderFlesh} />
           <ArtistGroup title="Space Sculpted By">
