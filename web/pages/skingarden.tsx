1443b6322993835a50ccefc7ba89fee8822e46f3
Max Bendick <maxbendick@gmail.com>
2021-06-17 09:40:23 -0700

    Day 2 zoom link

--- web/pages/skingarden.tsx
+++ web/pages/skingarden.tsx
@@ -151,7 +151,7 @@ const Header: React.FC = () => {
         </HeaderLink>
         <HeaderLink
           zIndexClassName="z-50"
-          href="https://us02web.zoom.us/j/86576915254"
+          href="https://us02web.zoom.us/j/86150716637"
           target="_blank"
         >
           Livestream{" "}
